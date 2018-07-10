195d4fc7bfbf5f83059e1e0d031c9204fb4e1619
teacherRay <34903301+teacherRay@users.noreply.github.com>
2018-07-10 19:32:20 +0700

    Course Details layout start

--- app/src/main/java/com/example/ray/recyclerviewtest/CourseDetailActivity.kt
+++ app/src/main/java/com/example/ray/recyclerviewtest/CourseDetailActivity.kt
@@ -1,12 +1,43 @@
 package com.example.ray.recyclerviewtest
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.activity_main.*
 
 class CourseDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_course_detail)
+        setContentView(R.layout.activity_main)
+        recyclerViewMain.layoutManager = LinearLayoutManager(this)
+        recyclerViewMain.adapter = CourseDetailAdapter()
     }
-}
+
+    private class CourseDetailAdapter : RecyclerView.Adapter<CourseLessonViewHolder>(){
+
+        override fun onCreateViewHolder(p0: ViewGroup, p1: Int): CourseLessonViewHolder {
+            val blueView = View(p0?.context)
+            blueView.setBackgroundColor(Color.BLUE)
+            blueView.minimumHeight = 50
+            return CourseLessonViewHolder(blueView)
+        }
+
+        override fun getItemCount(): Int {
+            return 3
+        }
+
+        override fun onBindViewHolder(p0: CourseLessonViewHolder, p1: Int) {
+
+        }
+
+        }
+    }
+    private class CourseLessonViewHolder(val customView: View): RecyclerView.ViewHolder(customView){
+
+    }
+
